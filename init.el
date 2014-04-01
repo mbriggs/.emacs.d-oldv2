2d4a4be0a2258811cbe51e1511a523d65fa85143
Matt Briggs <matt@mattbriggs.net>
2014-04-01 19:15:26 -0400

    switch to mapc

--- init.el
+++ init.el
@@ -4,25 +4,25 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/elisp"))
 
 
-(mapcar 'require '(init-bootstrap
-                   init-appearance
+(mapc 'require '(init-bootstrap
+                 init-appearance
 
-                   init-ido
-                   init-evil
-                   init-completion
-                   init-projectile
-                   init-magit
-                   init-expand-region
-                   init-web
-                   init-parens
-                   init-modeline
-                   init-markdown
-                   init-js
-                   init-flycheck
-                   init-help
+                 init-ido
+                 init-evil
+                 init-completion
+                 init-projectile
+                 init-magit
+                 init-expand-region
+                 init-web
+                 init-parens
+                 init-modeline
+                 init-markdown
+                 init-js
+                 init-flycheck
+                 init-help
 
-                   init-keymaps
-                   init-settings))
+                 init-keymaps
+                 init-settings))
 
 (require 'server)
 (unless (server-running-p)
