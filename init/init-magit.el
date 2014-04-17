4f38d3f0b1d1696b9eec911d577429f9a095e8b4
Matt Briggs <matt@mattbriggs.net>
2014-04-17 14:00:13 -0400

    hl-diff

--- init/init-magit.el
+++ init/init-magit.el
@@ -2,10 +2,12 @@
 
 (quelpa 'magit)
 (quelpa 'yagist)
+(quelpa 'diff-hl)
 (setq magit-emacsclient-executable "/usr/local/bin/emacsclient")
 
 (global-set-key (kbd "<f1>") 'magit-status)
 (define-key evil-normal-state-map (kbd "<f4>") 'magit-checkout)
+(global-diff-hl-mode +1)
 
 (add-hook 'git-commit-mode-hook
           '(lambda () (evil-emacs-state 1)))
