c6ee6d85be030814019275b76becbe4545e40427
Matt Briggs <matt@mattbriggs.net>
2014-04-19 18:34:51 -0400

    move customize stuff out of init.el

--- init.el
+++ init.el
@@ -44,31 +44,5 @@
 (require 'server)
 (unless (server-running-p)
   (server-start))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(column-enforce-face ((t (:background "#3a4055"))))
- '(company-preview ((t (:foreground "#d4cfc9"))))
- '(company-preview-common ((t (:foreground "controlShadowColor"))))
- '(company-preview-search ((t (:inherit company-preview))))
- '(company-scrollbar-bg ((t (:inherit company-tooltip :background "#3a4055"))))
- '(company-scrollbar-fg ((t (:background "#2b2b2b"))))
- '(company-tooltip ((t (:background "#5a647e" :foreground "#f4f1ed"))))
- '(company-tooltip-common ((t (:inherit company-tooltip))))
- '(company-tooltip-common-selection ((t (:inherit company-tooltip-selection :foreground "#f9f7f3"))))
- '(company-tooltip-selection ((t (:inherit company-tooltip :background "#6d9cbe"))))
- '(show-paren-match ((t (:background "#5a647e" :foreground "#d4cfc9"))))
- '(sp-show-pair-enclosing ((t (:background "#5a647e" :foreground "#d4cfc9"))))
- '(sp-show-pair-match-face ((t (:background "#5a647e" :foreground "#d4cfc9"))))
- '(trailing-whitespace ((t (:background "#3a4055")))))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(js3-auto-indent-p t)
- '(js3-highlight-external-variables nil)
- '(js3-highlight-level 3)
- '(session-use-package t nil (session)))
+(setq custom-file "~/.emacs.d/init/init-custom.el")
+(load custom-file)
