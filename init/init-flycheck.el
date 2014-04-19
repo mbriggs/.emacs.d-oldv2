ccfb80f8c824e35703f271cede2a9e508037b077
Matt Briggs <matt@mattbriggs.net>
2014-04-19 12:02:08 -0400

    run flycheck more often

--- init/init-flycheck.el
+++ init/init-flycheck.el
@@ -7,6 +7,10 @@
 (add-hook 'python-mode-hook 'flycheck-mode)
 (add-hook 'emacs-lisp-mode-hook 'flycheck-mode)
 (add-hook 'lisp-interaction-mode-hook 'flycheck-mode)
-(add-hook 'js2-mode-hook 'flycheck-mode)
+(add-hook 'js3-mode-hook 'flycheck-mode)
+(add-hook 'enh-ruby-mode-hook 'flycheck-mode)
 (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
 (setq flycheck-indication-mode nil)
+
+(add-hook 'flycheck-error-list-mode-hook
+          '(lambda () (evil-emacs-state 1)))
