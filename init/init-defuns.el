abf5ffdc58a9b666df6e58d100d33dad3dd37f2e
Matt Briggs <matt@mattbriggs.net>
2014-04-30 12:59:23 -0400

    delete-this-file-and-buffer

--- init/init-defuns.el
+++ init/init-defuns.el
@@ -15,6 +15,18 @@
   (interactive)
   (ido-find-file-in-dir (projectile-project-root)))
 
+(defun delete-this-buffer-and-file ()
+  "Removes file connected to current buffer and kills buffer."
+  (interactive)
+  (let ((filename (buffer-file-name))
+        (buffer (current-buffer))
+        (name (buffer-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (when (yes-or-no-p "Are you sure you want to remove this file? ")
+        (delete-file filename)
+        (kill-buffer buffer)
+        (message "File '%s' successfully removed" filename)))))
 
 (defun duplicate-line ()
   (interactive)
