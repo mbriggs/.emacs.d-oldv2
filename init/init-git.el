d0838a85e164462a4a254177cf807fc37066893a
Matt Briggs <matt@mattbriggs.net>
2014-06-16 20:22:25 -0400

    insert branch prefix into each commit work process....

--- init/init-git.el
+++ init/init-git.el
@@ -6,6 +6,18 @@
 (quelpa '(open-github-from-here :fetcher github :repo "mbriggs/emacs-open-github-from-here"))
 (setq magit-emacsclient-executable "/usr/local/bin/emacsclient")
 
+(add-hook 'git-commit-mode-hook 'insert-ticket-number-from-branch-name)
+
+(defun insert-ticket-number-from-branch-name ()
+  (evil-insert-state)
+  (let* ((current-branch (car (vc-git-branches)))
+         (ticket-number (replace-regexp-in-string ".+\\(/.*\\)$" ""
+                                                  current-branch nil nil 1)))
+    (when (string-match "^.+/" current-branch)
+      (newline)
+      (insert (concat "Ticket: " ticket-number))
+      (goto-char (point-min)))))
+
 (global-set-key (kbd "<f1>") 'magit-status)
 (define-key evil-normal-state-map (kbd "<f4>") 'magit-checkout)
 (global-diff-hl-mode +1)
